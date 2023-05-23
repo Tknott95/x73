3d640e5ba3997fb04760a2421557366a605fe3db
tknott95 <tknott95@hotmail.com>
2023-05-22 20:53:24 -0600

    working T-L-A with webpack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Blockfrost } from "lucid-cardano";
-import { Lucid } from "https://deno.land/x/lucid@0.10.5/mod.ts";
+import { Blockfrost, Lucid } from "lucid-cardano";
 
 @Component({
   selector: 'x73-root',
